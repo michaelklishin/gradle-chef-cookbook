40d0cc93f5cef5784822591d4504f7f4b20f4b99
Michael Klishin <michael@novemberain.com>
2013-08-22 02:16:10 +0400

    Gradle 1.7

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
-default[:gradle][:version] = "1.6"
+default[:gradle][:version] = "1.7"
 default[:gradle][:home_dir] = "/usr/local/gradle"
 default[:gradle][:release_url]       = "http://services.gradle.org/distributions/gradle-:version:-bin.zip"
