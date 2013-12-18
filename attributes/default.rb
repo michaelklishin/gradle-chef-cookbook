92b078c26934ba74fc01b804d9c056c8d955acc3
Michael Klishin <michael@clojurewerkz.org>
2013-12-18 17:24:18 +0300

    Gradle 1.10

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
-default[:gradle][:version] = "1.9"
+default[:gradle][:version] = "1.10"
 default[:gradle][:home_dir] = "/usr/local/gradle"
 default[:gradle][:release_url]       = "http://services.gradle.org/distributions/gradle-:version:-bin.zip"
