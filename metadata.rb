643db8e3b93f03e51bcf35253cef9a6a8b70f8d6
eran.zimbler <eran.zimbler@avg.com>
2013-12-19 12:04:52 +0200

    Fixing maintainer and version

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,9 @@
-maintainer       "nobody"
-maintainer_email "dunno@original"
-license          "Apache 2.0"
+maintainer       "Michael S. Klishin"
+maintainer_email "michael@clojurewerkz.org"
+license          "MIT"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.0"
+version          "1.0.0"
 
 description      "Installs Gradle"
-
-
 recipe           "gradle::tarball",              "installs gradle from tarball"
 
