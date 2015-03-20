e78328298dffe0b26596ec70d3cea6ab8a1e445d
Michael Klishin <michael@novemberain.com>
2015-03-20 22:41:48 +0100

    Provision 2.3 by default

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
-default[:gradle][:version] = "2.1"
+default[:gradle][:version] = "2.3"
 default[:gradle][:home_dir] = "/usr/local/gradle"
 default[:gradle][:release_url]       = "http://services.gradle.org/distributions/gradle-:version:-bin.zip"
