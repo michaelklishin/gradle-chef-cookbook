027601ab3993429aa4d884b220a02cd34657d533
iluretar <robertocuellarg@gmail.com>
2015-10-26 13:26:43 -0400

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default[:gradle][:name] = "gradle"
 default[:gradle][:version] = "2.6"
-default[:gradle][:home_dir] = "/usr/local/gradle"
+default[:gradle][:home_dir] = "/usr/local"
 default[:gradle][:release_url]       = "http://services.gradle.org/distributions/gradle-#{node[:gradle][:version]}-bin.zip"
