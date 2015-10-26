8ce08461e59235fde7d392133506b9410501dfb7
iluretar <robertocuellarg@gmail.com>
2015-10-25 23:50:41 -0400

    Add name to metadata

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "michael@clojurewerkz.org"
 license          "MIT"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
-
+name             "gradle"
 description      "Installs Gradle"
 recipe           "gradle::tarball",              "installs gradle from tarball"
 
