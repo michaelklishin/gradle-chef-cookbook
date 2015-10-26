be2453ba9657570a0e19c9e10db2ad0d6c0d64b9
iluretar <robertocuellarg@gmail.com>
2015-10-26 00:59:19 -0400

    Add name attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
+default[:gradle][:name] = "gradle"
 default[:gradle][:version] = "2.6"
 default[:gradle][:home_dir] = "/usr/local/gradle"
 default[:gradle][:release_url]       = "http://services.gradle.org/distributions/gradle-:version:-bin.zip"
