999c445d2f17778daf846b387461edf7fc2e00ac
Joseph Vartuli <joevartuli@gmail.com>
2015-10-30 16:17:23 +1100

    Updated to comply with Chef lint tool foodcritic

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default[:gradle][:name]              = "gradle"
-default[:gradle][:version]           = "2.8"
-default[:gradle][:home_dir]          = "/usr/local"
-default[:gradle][:release_url]       = "http://services.gradle.org/distributions/gradle-#{node[:gradle][:version]}-bin.zip"
+default['gradle']['name'] = 'gradle'
+default['gradle']['version'] = '2.8'
+default['gradle']['home_dir'] = '/usr/local'
+default['gradle']['release_url'] = "http://services.gradle.org/distributions/gradle-#{node['gradle']['version']}-bin.zip"
